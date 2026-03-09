6456ff9a4f0f8daced9a24212c6eeae95598a9e6
Deepak-vm <Deepak-vm@users.noreply.github.com>
2026-03-09 08:17:44 +0000

    feat(output-parsers): implement StrOutputParser, JsonOutputParser, and PydanticOutputParser

---
+++ 6-output-parsers/stroutputparser.py
@@ -0,0 +1,32 @@
+from langchain_core.output_parsers import StrOutputParser
+from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id='meta-llama/Llama-3.1-8B-Instruct', task="text-generation")
+
+model = ChatHuggingFace(llm=llm)
+
+# detailed report
+template1 = PromptTemplate(
+    template='Write a detailed report on {topic}', 
+    input_variables=['topic']
+)
+
+# Summary 
+template2= PromptTemplate(
+    template="Write a 5 line Summary on the following text. \n {text}",
+    input_variables=['text']
+)
+
+parser= StrOutputParser()
+
+chain = template1 | model | parser | template2 | model | parser
+
+result= chain.invoke({'topic':'black hole'})
+
+
+print(result)
